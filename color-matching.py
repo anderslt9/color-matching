9c88187d422edb40d3ca3c86c61691e3f0f57a9c
anderslt9 <anderslt@purdue.edu>
2024-12-26 22:20:39 -0500

    display_img now works correctly

--- color-matching.py
+++ color-matching.py
@@ -3,7 +3,7 @@ from skimage import transform
 from copy import deepcopy
 import inspect
 
-IMAGE_PATH = "/imgs_out/"
+IMAGE_PATH = "imgs_out/"
 
 def line_num():
   return inspect.currentframe().f_back.f_lineno
@@ -11,8 +11,11 @@ def line_num():
 def err_print(*args):
   print(f"\033[93m{args[0]} at line {line_num()}\033[0m", *args[1:])
 
+def float_to_int_img(img):
+  return (img * 255).astype(int)
+
 class Image_Process:
-  def __init__(self, img_path, reshape_size=None):
+  def __init__(self, img_path):
     self.base_img = cv.imread(img_path)
     self.reset_img()
   
@@ -36,8 +39,8 @@ class Image_Process:
     if type(reshape_size) != tuple: 
       if type(reshape_size) != float and type(reshape_size) != int: 
         err_print("Invalid input for reshape_size")
-      assert type(reshape_size) == float or type(reshape_size) == int
-      self.img = transform.resize(self.img, (int(self.img.shape[0] * reshape_size), int(self.img.shape[1] * reshape_size), 3))
+        return
+      self.img = transform.resize(self.img, (int(self.img.shape[0] * reshape_size), int(self.img.shape[1] * reshape_size), 3), preserve_range=True).astype(int)
       self.img_size = self.img.shape[:2]
       return 
   
@@ -48,7 +51,7 @@ class Image_Process:
     
     # if reshape size has no negative number associated, resize directly 
     if reshape_size[0] > 0 and reshape_size[1] > 0: 
-      self.img = transform.resize(self.img, (*reshape_size,3)) 
+      self.img = transform.resize(self.img, (*reshape_size,3), preserve_range=True).astype(int)
       self.img_size = self.img.shape[:2]
       return
     
@@ -60,10 +63,10 @@ class Image_Process:
     # resize negative size according to positive size
     if reshape_size[0] > 0: 
       ratio = reshape_size[0] / self.img.shape[0]
-      self.img = transform.resize(self.img, (reshape_size[0], int(self.img.shape[1] * ratio), 3))
+      self.img = transform.resize(self.img, (reshape_size[0], int(self.img.shape[1] * ratio), 3), preserve_range=True).astype(int)
     else:
       ratio = reshape_size[1] / self.img.shape[1]
-      self.img = transform.resize(self.img, (int(self.img.shape[0] * ratio), self.img.shape[1], 3))
+      self.img = transform.resize(self.img, (int(self.img.shape[0] * ratio), self.img.shape[1], 3), preserve_range=True).astype(int)
     
     self.img_size = self.img.shape[:2]
   
@@ -72,7 +75,8 @@ class Image_Process:
 
 
 if __name__ == "__main__":
-  img_proc = Image_Process()
+  img_proc = Image_Process("imgs-test/spirit_happy.jpg")
   img_proc.reshape_img((64,-1))
+  img_proc.display_img("spirit_happy_64X64")
   
   
